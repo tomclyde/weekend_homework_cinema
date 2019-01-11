512066f91e3a70f2a797a7f3cd4377d86769491e
Tom C <tclyde@hotmail.com>
2019-01-11 11:06:10 +0000

    customer created

--- console.rb
+++ console.rb
@@ -10,7 +10,17 @@ Film.delete_all()
 film1 = Film.new({'title' => 'Ralph breaks the Internet', 'price' => 6.25})
 film2 = Film.new({'title' => 'Mary Queen of Scots', 'price' => 8.50})
 film3 = Film.new({'title' => 'The Grinch', 'price' => 0.50})
+film4 = Film.new({'title' => 'Jaws', 'price' => 23.75})
 
 film1.save
 film2.save
 film3.save
+film4.save
+
+customer1 = Customer.new({'name' => 'Tom Clyde', 'funds' => 100.00})
+customer2 = Customer.new({'name' => 'Louise Clyde', 'funds' => 8.00})
+customer3 = Customer.new({'name' => 'Ryan Clyde', 'funds' => 0.25})
+
+customer1.save
+customer2.save
+customer3.save
---
+++ models/customer.rb
@@ -0,0 +1,59 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_accessor :name, :funds
+  attr_reader :id
+
+  def initialize(customer_details)
+    @id = customer_details['id'].to_i if customer_details['id']
+    @name = customer_details['name']
+    @funds = customer_details['funds']
+  end
+
+
+  def save()
+    sql = "INSERT INTO customers
+    (
+      name,
+      funds
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@name, @funds]
+    customers = SqlRunner.run( sql, values ).first
+    @id = customers['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    values = []
+    customers = SqlRunner.run(sql, values)
+    result = customers.map { |customer| Customer.new( customer ) }
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "
+    UPDATE customers SET (
+      name,
+      funds
+    ) =
+    (
+      $1,$2
+    )
+    WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql,values)
+  end
+
+end
