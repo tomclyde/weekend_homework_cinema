66956b7aa0b40a39069b76d275ddd0f538d9683b
Tom C <tclyde@hotmail.com>
2019-01-11 11:51:23 +0000

    ticket created

--- console.rb
+++ console.rb
@@ -24,3 +24,15 @@ customer3 = Customer.new({'name' => 'Ryan Clyde', 'funds' => 0.25})
 customer1.save
 customer2.save
 customer3.save
+
+ticket1 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer1.id})
+ticket2 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer1.id})
+ticket3 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer2.id})
+ticket4 = Ticket.new({'film_id' => film2.id, 'customer_id' => customer2.id})
+ticket5 = Ticket.new({'film_id' => film4.id, 'customer_id' => customer2.id})
+
+ticket1.save
+ticket2.save
+ticket3.save
+ticket4.save
+ticket5.save
---
+++ models/ticket.rb
@@ -0,0 +1,56 @@
+require_relative("../db/sql_runner")
+
+class Ticket
+
+  attr_reader :id
+  attr_accessor :film_id, :customer_id
+
+  def initialize( ticket_details )
+    @id = ticket_details['id'].to_i if ticket_details['id']
+    @film_id = ticket_details['film_id'].to_i
+    @customer_id = ticket_details['customer_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO tickets
+    (
+      film_id,
+      customer_id
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@film_id, @customer_id]
+    tickets = SqlRunner.run( sql,values ).first
+    @id = tickets['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    tickets = SqlRunner.run(sql)
+    result = tickets.map { |ticket| Ticket.new( ticket ) }
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
+  end
+
+  def update()
+    sql = "
+    UPDATE tickets SET (
+      film_id,
+      customer_id
+    ) =
+    (
+      $1,$2
+    )
+    WHERE id = $3"
+    values = [@film_id, @customer_id, @id]
+    SqlRunner.run(sql,values)
+  end
+
+end
